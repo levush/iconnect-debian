69e54c11c278e5e04706ddbe42f20c4f7a8ddd5c
Lukas Steiner <lukas.steiner@steinheilig.de>
2018-08-10 12:58:35 +0200

    fix kernel build deb

--- kernel_functions.sh
+++ kernel_functions.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabi-
+export LOADADDR=0x00008000
+
 # download and extract linux kernel
 function kernel_download {
     pp INFO "Download linux kernel $LINUX_KERNEL_VERSION"
@@ -38,15 +42,13 @@ function kernel_patch {
 
 # build linux kernel
 function kernel_build {
-    export ARCH=arm
-    export CROSS_COMPILE=$WORK_DIR/toolchain/arm/bin/arm-none-eabi-
     cd kernel/$LINUX_KERNEL_DIR
     pp INFO "Build linux kernel $LINUX_KERNEL_VERSION"
 
     pp INFO "Build dtbs"
     make dtbs
     pp INFO "Build uImage"
-    make LOADADDR=0x00008000 uImage
+    make uImage
     pp INFO "Build modules"
     make modules
     pp INFO "Build firmware"
@@ -54,33 +56,9 @@ function kernel_build {
     cd $WORK_DIR
 }
 
-function kernel_build_all {
-    export ARCH=arm
-    export CROSS_COMPILE=$WORK_DIR/toolchain/arm/bin/arm-none-eabi-
-    cd kernel/$LINUX_KERNEL_DIR
-    pp INFO "Build linux kernel $LINUX_KERNEL_VERSION"
-
-    make -j $COMPILE_CORES
-    pp INFO "Build uImage"
-    make LOADADDR=0x00008000 uImage
-    cd $WORK_DIR
-}
-
-function kernel_build_deb {
-    export ARCH=arm
-    export CROSS_COMPILE=$WORK_DIR/toolchain/arm/bin/arm-none-eabi-
-    cd kernel/$LINUX_KERNEL_DIR
-    pp INFO "Build kernel deb packages"
-
-    make -j $COMPILE_CORES deb-pkg LOCALVERSION=-iconnect KDEB_PKGVERSION=1
-    cd $WORK_DIR
-}
-
 # install linux kernel
 function kernel_install {
     check_root_privileges
-    export ARCH=arm
-    export CROSS_COMPILE=$WORK_DIR/toolchain/arm/bin/arm-none-eabi-
 
     mkdir -p image/fs-kernel/boot image/fs-kernel/usr
     pp INFO "Install linux kernel"
@@ -89,11 +67,14 @@ function kernel_install {
     cd kernel/$LINUX_KERNEL_DIR
     pp INFO "Install modules"
     make INSTALL_MOD_PATH=$WORK_DIR/image/fs-kernel modules_install
-    # pp INFO "Install headers"
-    # make INSTALL_HDR_PATH=$WORK_DIR/image/fs-kernel/usr headers_install
-    # cd $WORK_DIR/image/fs-kernel/lib/modules/$LINUX_KERNEL_VERSION-iconnect
-    # rm build source
-    # ln -s ../../../usr/include build
-    # ln -s ../../../usr/include source
+    cd $WORK_DIR
+}
+
+# build debian kernel packages
+function kernel_build_deb {
+    cd kernel/$LINUX_KERNEL_DIR
+    pp INFO "Build kernel deb packages"
+
+    make -j$COMPILE_CORES KBUILD_IMAGE=uImage KDEB_PKGVERSION=$(date "+%d%m%y") deb-pkg
     cd $WORK_DIR
 }
